37020ab9e271f9b95dcb71ca7325c27037fa7213
Oishwarjya Banerjee <ohitsmeoishee@gmail.com>
2022-09-28 06:32:08 +0530

    added sound/ tone

--- neo_activation.py
+++ neo_activation.py
@@ -3,8 +3,11 @@ from adafruit_circuitplayground import cp
 cp.pixels.brightness = 0.05
 
 while True:
-    cp.pixels[0] = (255, 0, 0)
-    cp.pixels[1] = (0, 255, 0)
-    cp.pixels[3] = (0, 255, 255)
-    cp.pixels[4] = (255, 255, 0)
-    cp.pixels[2] = (255, 0, 255)
+    if cp.button_b:
+        cp.pixels[0] = (255, 0, 0)
+        cp.pixels[1] = (0, 255, 0)
+        cp.pixels[3] = (0, 255, 255)
+        cp.pixels[4] = (255, 255, 0)
+        cp.pixels[2] = (255, 0, 255)
+        cp.play_tone(100, 2)  # (volume, time)
+        cp.play_tone(180, 1)
