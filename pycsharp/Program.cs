7521db84a5dbd0743214172264690b2d4cc31840
Oishwarjya Banerjee <ohitsmeoishee@gmail.com>
2022-10-05 03:46:46 +0530

    Files being read from python file

--- pycsharp/Program.cs
+++ pycsharp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO; 
+using System.Diagnostics; 
 
 namespace pycsharp
 {
@@ -6,7 +8,29 @@ namespace pycsharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string FileName = "scripts.py"; 
+            ProcessStartInfo ProcessInfo = new ProcessStartInfo("python3"); 
+
+            // make sure we can read the output from stdout 
+            ProcessInfo.UseShellExecute = false; 
+            ProcessInfo.RedirectStandardOutput = true; 
+
+            ProcessInfo.Arguments = FileName;
+            Process myProcess = new Process(); 
+
+            // assign start information to the process 
+            myProcess.StartInfo = ProcessInfo; 
+
+            // start the process 
+            myProcess.Start(); 
+
+            StreamReader myStreamReader = myProcess.StandardOutput; 
+            string myString = myStreamReader.ReadLine(); 
+            myProcess.WaitForExit(); 
+            myProcess.Close();
+            Console.WriteLine("Value received from script: " + myString); 
+        
+
         }
     }
 }
