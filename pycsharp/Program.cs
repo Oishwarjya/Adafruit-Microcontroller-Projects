a96e8723f37b373ec569f7a783cfc8672edf7246
Oishwarjya Banerjee <ohitsmeoishee@gmail.com>
2022-10-05 03:13:52 +0530

    Basic C# setup for VS Code

---
+++ pycsharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pycsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
